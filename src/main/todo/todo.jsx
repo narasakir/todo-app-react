5c780e1c4144591d829ae5fd0c28b7be60042b75
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-16 10:20:12 -0200

    eventos no formulario

--- src/main/todo/todo.jsx
+++ src/main/todo/todo.jsx
@@ -3,13 +3,30 @@ import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 export default class Todo extends Component {
+  constructor(props){
+    super(props)
+    this.state = {description : '', list: []}
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+  handleAdd(){
+    console.log(this)
+  }
+
+  handleChange(e){
+    this.setState({...this.state, description: e.target.value})
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas"  small="Cadastro"/>
-        <TodoForm />
-        <TodoList />
+        <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}/>
+        <TodoList  />
       </div>
     )
   }
+
 }
